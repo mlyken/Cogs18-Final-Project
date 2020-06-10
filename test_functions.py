e5539aeb4a0bf5dcd903888b008f528f48cd1601
Mike Lyken <55967598+mlyken@users.noreply.github.com>
2020-06-09 19:00:43 -0700

    update

--- test_functions.py
+++ test_functions.py
@@ -3,53 +3,69 @@ import string
 import re
 from functions import count_genre, count_year, remove_year,remove_extras, count_rating
 
-file = pd.read_csv("IMDB_Titles.csv")
-titles = file.Title.unique()
-initial = list(file['Year'])
-genre1 = list(file['Genre'])
-rating = list(file['Rating (Of 10)'])
 
 
-def count_genre(genre1):
+def test_count_genre():
 
 	test1 = ["action",'thriller','sci-fi', 'action']
+	test2 = ["action",'thriller','sci-fi', 'action','horror']
+
 	assert callable(count_genre)
-	assert isinstance(genre1,list)
-	assert len(genre1) == 100
-	assert isinstance(genre1[2], str)
+	assert isinstance(test1,list)
+	assert len(test1) == 4
+	assert isinstance(test1[2], str)
 
 	assert count_genre(test1) == {'action': 2, 'thriller': 1, 'sci-fi': 1}
+	assert count_genre(test2) == {'action': 2, 'thriller': 1, 'sci-fi': 1, 'horror': 1}
 
-def count_year(initial):
+def test_count_year():
+	initial = [2019,2019,2011,2018,2017]
+	initial2 = [2019,2019,2011]
+
 	assert callable(count_year)
 	assert isinstance(initial, list)
-	assert len(initial) == 100
+	assert len(initial) == 5
 	assert isinstance(initial[0], int)
 
-	assert count_year([2019,2019,2011,2018,2017]) == {2019: 2, 2011: 1, 2018: 1, 2017: 1}
+	assert count_year(initial) == {2019: 2, 2011: 1, 2018: 1, 2017: 1}
+	assert count_year(initial2) == {2019: 2, 2011: 1}
 
-def remove_extras(titles):
+def test_remove_extras():
+	
+	titles = ['Grownups ()']
+	titles2 = ['Grom () days']
+	titles3 = ['Grom (99) days']
+
 	assert callable(remove_extras)
-	assert len(titles) == 100
+	assert len(titles) == 1
 	assert isinstance(titles[0], str)
 
-	assert remove_extras(['Grownups ()']) == ['Grownups ', '']
-	assert remove_extras(['Grom () days']) == ['Grom ', ' days']
-	assert remove_extras(['Grom (99) days']) == ['Grom (99) days']
+	assert remove_extras(titles) == ['Grownups ', '']
+	assert remove_extras(titles2) == ['Grom ', ' days']
+	assert remove_extras(titles3) == ['Grom (99) days']
 
 
-def remove_year(titles):
+def test_remove_year():
+	film = ['Grownups (2010)']
+	film2 = ['Grownups (2010)','Grownups 2 (2013)']
+
 	assert callable(remove_year)
-	assert len(titles) == 100
+	assert len(film) == 1
 
-	assert isinstance(titles[5], str)
-	assert remove_year(['Grownups (2010)']) == ['Grownups ()']
+	assert isinstance(film[0], str)
+	assert remove_year(film) == ['Grownups ()']
+	assert remove_year(film2) == ['Grownups ()', 'Grownups  ()']
 
-def count_rating(rating):
+def test_count_rating():
+	rating = ['7.8','9.2','7.8','6.6']
+	rating2 = ['7.8','9.2','7.8','6.6',6.6]
+
 	assert callable(count_rating)
 	assert isinstance(rating, list)
-	assert len(rating) == 100
-	assert isinstance(rating[0], float)
+	assert len(rating) == 4
+	assert isinstance(rating[0], str)
+	assert isinstance(rating2[4], float)
 
-	assert count_rating(['7.8','9.2','7.8','6.6']) == {'7.8': 2, '9.2': 1, '6.6': 1}
-	assert count_rating(['7.8','9.2','7.8','6.6',6.6]) == {'7.8': 2, '9.2': 1, '6.6': 1, 6.6: 1}
+
+	assert count_rating(rating) == {'7.8': 2, '9.2': 1, '6.6': 1}
+	assert count_rating(rating2) == {'7.8': 2, '9.2': 1, '6.6': 1, 6.6: 1}
